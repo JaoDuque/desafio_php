e02a3d326de8b5527357504c89a1f50ebfb23752
JaoDuque <107492575+JaoDuque@users.noreply.github.com>
2022-08-24 11:25:07 -0300

    Add files via upload

---
+++ notas.php
@@ -0,0 +1,26 @@
+<?php
+
+echo "-----------------------------REGRAS---------------------------------------------";
+echo "|          Todas as notas maiores do que 10 serão consideras 10                |";
+echo "|    Todas as notas negativas ou não digitadas serão consideradas 0            |";
+echo "--------------------------------------------------------------------------------";
+
+    echo "Informe a turma:". PHP_EOL;
+    $turma = fgets(STDIN);
+   
+    echo "Digite a quantidade de alunos". PHP_EOL;
+    $qtdalunos = fgets(STDIN);
+ 
+    echo "-------------------------------------------------------------------------------";
+    echo "|                             Digite as notas                                 |";
+    echo "-------------------------------------------------------------------------------";
+    
+   for($i = 0; $i = $numaluno ; $i++){
+    echo "Nota $numaluno =". PHP_EOL;
+    $nota1 = fgets(STDIN);
+   }
+
+double $nota1 
+double $nota2
+double $mediaAluno = ($nota1 + $nota2)/2
+double $mediaGeral = $qtdalunos 
