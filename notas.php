7bc41fc09c32b3f34c73ea30b99f585c5676d1cf
Duque <107492575+JaoDuque@users.noreply.github.com>
2022-08-26 10:54:09 -0300

    Add files via upload
    Finalizado.

--- notas.php
+++ notas.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "-----------------------------REGRAS---------------------------------------------".PHP_EOL;
+echo "----------------------------------REGRAS----------------------------------------".PHP_EOL;
 echo "|          Todas as notas maiores do que 10 serão consideras 10                |".PHP_EOL;
 echo "|    Todas as notas negativas ou não digitadas serão consideradas 0            |".PHP_EOL;
 echo "--------------------------------------------------------------------------------".PHP_EOL;
@@ -10,11 +10,13 @@ echo "--------------------------------------------------------------------------
    
     echo "Digite a quantidade de alunos:".PHP_EOL;
      $qtdalunos = fgets(STDIN);
-    
+     
     echo "-------------------------------------------------------------------------------".PHP_EOL;
     echo "|                             Digite as notas                                 |".PHP_EOL;
     echo "-------------------------------------------------------------------------------".PHP_EOL;
     
+    $notas = [];
+
    for($i = 0; $i < $qtdalunos; $i++){
     $alunoNum = $i+1;
     echo "Aluno $alunoNum=".PHP_EOL;
@@ -34,14 +36,13 @@ echo "--------------------------------------------------------------------------
 
    $mediaGeral = 0;
    foreach ($notas as $nota) {
-       $mediaGeral += $notas['prova1'] + $notas['prova2'];
+       $mediaGeral += $nota['prova1'] + $nota['prova2'];
    }
    $media = $mediaGeral / ($qtdalunos*2);
    
-   
    $mediaAlunos = 0;
    foreach ($notas as $nota) {
-       $mediaAlunos += $notas['prova1'] + $notas['prova2'];
+       $mediaAlunos += $nota['prova1'] + $nota['prova2'];
        $mediaA = $mediaAlunos / 2;
    }
    $final = $mediaA / $qtdalunos;
@@ -49,11 +50,19 @@ echo "--------------------------------------------------------------------------
    echo "-------------------------------------------------------------------------------".PHP_EOL;
    echo "|Turma: $turma".PHP_EOL;
    echo "-------------------------------------------------------------------------------".PHP_EOL;
-   echo "|Média de todas as notas: $mediaGeral".PHP_EOL;
+   echo "|Média de todas as notas: $media".PHP_EOL;
    echo "-------------------------------------------------------------------------------".PHP_EOL;
    echo "|Média de todas as médias: $final".PHP_EOL;
    echo "-------------------------------------------------------------------------------".PHP_EOL;
    echo "|                               MÉDIA DOS ALUNOS                              |".PHP_EOL;
    echo "-------------------------------------------------------------------------------".PHP_EOL;
-
    
+   foreach($notas as $i => $nota){
+       $alunoNum = $i+1;
+       $mediaAlunos = $nota['prova1'] + $nota['prova2'];
+       $mediaA = $mediaAlunos / 2;
+    
+    echo "|Aluno $alunoNum ".PHP_EOL;
+    echo "|Média = $mediaA".PHP_EOL;
+    echo "-------------------------------------------------------------------------------".PHP_EOL;
+   }
